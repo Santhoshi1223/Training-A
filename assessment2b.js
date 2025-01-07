9da2f183ff7306e991876be05260787e111c60c4
Santhoshi <santhutangellapalli@gmail.com>
2025-01-07 20:18:40 +0530

    Pushing files to the github

---
+++ assessment2b.js
@@ -0,0 +1,17 @@
+// Assignment given by trainer
+
+//If else Ladder                                                                                                                                                       // Taking user input
+var a = parseInt(prompt("Enter the first value (a):"));
+var b = parseInt(prompt("Enter the second value (b):"));
+var c = parseInt(prompt("Enter the third value (c):"));
+
+// If-Else Ladder
+if (a > b && a > c) {
+  console.log("a is the greatest: " + a);
+} else if (b > a && b > c) {
+  console.log("b is the greatest: " + b);
+} else if (c > a && c > b) {
+  console.log("c is the greatest: " + c);
+} else {
+  console.log("There is a tie.");
+}
---
+++ operators.js
@@ -0,0 +1,42 @@
+//! arithmatic operator
+// console.log(10+10); //addition
+// console.log("10" + 10); //1010 concatenation 
+// console.log(10+40+"Hi"); //50Hi
+
+// console.log("JS"+50+50); //JS5050
+// console.log("JS"+(50+50)); //JS100
+
+
+// console.log(10-5); //5
+// console.log("10" - 5); //5 (applies typecasting)
+// console.log("10" - "Hi"); //NaN error (Not a Number)
+// console.log("10" - "5"); //5
+// console.log(5*5); //25
+// console.log("5" * 5); //25 (typecasting)
+
+// console.log(10/2);
+// console.log("10"/2);
+
+// console.log(10%2); 
+
+// console.log(2**5);
+// console.log("2" **5);
+
+// let a=10;
+// console.log(++a); //11 (pre-increment)(1+a)
+// console.log(a++); //11 (post-increment)(a+1)
+// console.log(a);
+
+// let b=10;
+// console.log(--b); //9 (pre-decrement)(1-b)
+// console.log(b--); //9 (post-decrement)(b-1)
+// console.log(b);
+
+// console.log(10 == 10); //true
+// console.log("Hi" == "Hi"); //true
+
+// console.log("10" === 10); //false
+
+// console.log(true && true); //true
+// console.log(true && false); //false
+// console.log(false&false);
